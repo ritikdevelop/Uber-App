a5e8f4953f01d429d4aa3619122dd7b9ffeb50fc
ritikdevelop <hrithikrana216@gmail.com>
2024-09-22 01:55:48 +0530

    Fourth Commit in Uber clone App

--- uber-clone/.expo/types/router.d.ts
+++ uber-clone/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(auth)\_layout` | `/(auth)\sign-in` | `/(auth)\sign-up` | `/(auth)\welcome` | `/(root)\(tabs)\_layout` | `/(root)\(tabs)\chat` | `/(root)\(tabs)\home` | `/(root)\(tabs)\profile` | `/(root)\(tabs)\rides` | `/(root)\_layout` | `/_sitemap`;
+      StaticRoutes: `/` | `/(auth)` | `/(auth)/sign-in` | `/(auth)/sign-up` | `/(auth)/welcome` | `/(root)` | `/(root)/(tabs)` | `/(root)/(tabs)/chat` | `/(root)/(tabs)/home` | `/(root)/(tabs)/profile` | `/(root)/(tabs)/rides` | `/(root)/chat` | `/(root)/home` | `/(root)/profile` | `/(root)/rides` | `/(tabs)` | `/(tabs)/chat` | `/(tabs)/home` | `/(tabs)/profile` | `/(tabs)/rides` | `/..\components\CustomButton` | `/..\constants\` | `/_sitemap` | `/chat` | `/home` | `/profile` | `/rides` | `/sign-in` | `/sign-up` | `/welcome`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
