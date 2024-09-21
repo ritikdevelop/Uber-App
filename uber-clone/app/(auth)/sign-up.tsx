4deb39cac870e5c9357b2e3c4069c823d6a1fd17
ritikdevelop <hrithikrana216@gmail.com>
2024-09-22 04:03:10 +0530

    Fifth commit in Uber Clone App

--- uber-clone/app/(auth)/sign-up.tsx
+++ uber-clone/app/(auth)/sign-up.tsx
@@ -1,14 +1,75 @@
-import { Text } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
+import { Link } from "expo-router";
+import React, { useState } from "react";
+import { Text, View, ScrollView, Image } from "react-native";
 
 const SignUp = () => {
-    return(
-        <SafeAreaView>
-            <Text>
-                SignUp
-            </Text>
-        </SafeAreaView>
-    )
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onSignUpPress = async () => {};
+
+  return (
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[150px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[170px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create Your Account
+          </Text>
+        </View>
+
+        <View className="p-5">
+          <InputField
+            label="Name"
+            placeholder="Enter your name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => setForm({ ...form, name: value })}
+          />
+          <InputField
+            label="Email"
+            placeholder="Enter email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignUpPress}
+            className="mt-6"
+          />
+
+          {/* OAuth */}
+
+          <OAuth />
+
+          <Link href="/sign-in"
+          className="tex-lg text-center text-general-200 mt-10">
+            <Text>Already have an account?</Text>
+            <Text className="text-primary-500">Log In</Text>
+          </Link>
+        </View>
+
+        {/* Verification Modal */}
+      </View>
+    </ScrollView>
+  );
 };
 
 export default SignUp;
--- uber-clone/components/CustomButton.tsx
+++ uber-clone/components/CustomButton.tsx
@@ -11,24 +11,24 @@ const getBgVariantStyle = (variant: ButtonProps["bgVariant"]) => {
       return "bg-green-500";
     case "outline":
       return "bg-transparent border-neutral-300 border-[0.5px]";
-      default:
-        return "bg-[#0286ff]";
+    default:
+      return "bg-[#0286ff]";
   }
 };
 const getTextVariantStyle = (variant: ButtonProps["textVariant"]) => {
-    switch (variant) {
-      case "primary":
-        return "text-black";
-      case "secondary":
-        return "bg-gray-100";
-      case "danger":
-        return "bg-red-100";
-        case "success":
-        return "bg-green-100";
-        default:
-          return "text-white";
-    }
-  };
+  switch (variant) {
+    case "primary":
+      return "text-black";
+    case "secondary":
+      return "bg-gray-100";
+    case "danger":
+      return "bg-red-100";
+    case "success":
+      return "bg-green-100";
+    default:
+      return "text-white";
+  }
+};
 const CustomButton = ({
   onPress,
   title,
@@ -46,7 +46,9 @@ ${getBgVariantStyle(bgVariant)} ${className}`}
     {...props}
   >
     {IconLeft && <IconLeft />}
-    <Text className={`text-lg font-bold ${getTextVariantStyle(textVariant)}`}>{title}</Text>
+    <Text className={`text-lg font-bold ${getTextVariantStyle(textVariant)}`}>
+      {title}
+    </Text>
     {IconRight && <IconRight />}
   </TouchableOpacity>
 );
